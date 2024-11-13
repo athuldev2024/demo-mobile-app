543aa35124dfe21ca4f2a919f22dc0afc57dceba
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-13 22:58:53 +0530

    remove unwanted code for now

--- Demo/babel.config.js
+++ Demo/babel.config.js
@@ -1,15 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'module:react-native-dotenv',
-    {
-      envName: 'APP_ENV', // Optional: the environment variable name
-      moduleName: '@env',
-      path: '.env',
-      blocklist: null,
-      allowlist: null,
-      safe: false,
-      allowUndefined: true,
-    },
-  ],
 };
