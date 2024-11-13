5d69e32e50b69942e74b694d7024c64eef4ca743
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-13 22:09:20 +0530

    in progress

--- Demo/babel.config.js
+++ Demo/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'module:react-native-dotenv',
+    {
+      envName: 'APP_ENV', // Optional: the environment variable name
+      moduleName: '@env',
+      path: '.env',
+      blocklist: null,
+      allowlist: null,
+      safe: false,
+      allowUndefined: true,
+    },
+  ],
 };
