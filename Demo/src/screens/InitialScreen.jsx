d0586941fc5bc0ee0d9ea3ef7dae3a020303a657
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-14 23:45:27 +0530

    in progress

--- Demo/src/screens/InitialScreen.jsx
+++ Demo/src/screens/InitialScreen.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
 import {Login, Register} from '../components';
 
 function InitialScreen() {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    AsyncStorage.getItem('userDetails').then(userDetails => {
+      if (userDetails) {
+        navigation.navigate('ProfileScreen');
+      }
+    });
+  });
 
   return (
     <Stack.Navigator initialRouteName="Login">
--- Demo/src/screens/ProfileScreen.jsx
+++ Demo/src/screens/ProfileScreen.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, StyleSheet, Dimensions} from 'react-native';
+import {CustomButton, Header} from '../components';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
+
+const {width} = Dimensions.get('window');
 
 function ProfileScreen() {
-  return <Text>ProfileScreen</Text>;
+  const navigation = useNavigation();
+
+  const logOutFunc = () => {
+    AsyncStorage.clear();
+    navigation.navigate('InitialScreen');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Header>Profile screen</Header>
+        <CustomButton title="" icon="logout" onPress={logOutFunc} />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: width * 0.09,
+    paddingVertical: width * 0.09,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    gap: 20,
+  },
+});
 
 export default ProfileScreen;
