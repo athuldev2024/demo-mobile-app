0eed678728d1a428b10dae858a0a247905f4fab7
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-20 16:45:38 +0530

    process screen - go back button

--- Demo/src/screens/ProcessScreen.jsx
+++ Demo/src/screens/ProcessScreen.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
+import {View, StyleSheet} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import {useNavigation} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {Message, Update} from '../components';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../constants/colors';
 
 function ProcessScreen() {
   const Stack = createNativeStackNavigator();
-  //   const navigation = useNavigation();
+  const navigation = useNavigation();
+
+  const goBackFunc = () => {
+    navigation.goBack();
+  };
 
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Message"
-        component={Message}
-        options={{headerShown: false}}
+    <View style={styles.container}>
+      <Icon
+        name="arrow-back"
+        onPress={goBackFunc}
+        size={30}
+        color={COLORS.primary}
+        style={styles.back}
       />
-      <Stack.Screen
-        name="Update"
-        component={Update}
-        options={{headerShown: false}}
-      />
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Message"
+          component={Message}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Update"
+          component={Update}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: '2%',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  back: {alignSelf: 'flex-start'},
+});
 
 export default ProcessScreen;
