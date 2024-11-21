169b75674c5f2ebf6d9ed0f8e0c3dfe5e6d9e93c
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-21 18:01:50 +0530

    delete message ping message

--- Demo/src/components/Message.jsx
+++ Demo/src/components/Message.jsx
@@ -1,9 +1,153 @@
-import React from 'react';
-import {Text} from 'react-native-paper';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useDispatch, useSelector} from 'react-redux';
+import {TextInput} from 'react-native-paper';
+import {
+  viewMessageData,
+  pingMessage,
+  deleteMessage,
+} from '../store/messageSlice';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import COLORS from '../constants/colors';
+
+const Card = ({item, userID}) => {
+  const dispatch = useDispatch();
+
+  const deleteMessageFunc = () => {
+    dispatch(
+      deleteMessage({
+        params: {
+          messageID: item.id,
+        },
+      }),
+    );
+  };
+
+  return (
+    <View
+      style={{
+        ...styles.card,
+        alignSelf: item.alignSelf,
+      }}>
+      <Text>{item.message}</Text>
+      <TouchableOpacity onPress={deleteMessageFunc}>
+        <Icon name="restore-from-trash" size={30} color={COLORS.primary} />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 function Message(props) {
-  console.log('Message PROPS: ', props.route.params);
-  return <Text>Message screen</Text>;
+  const [ping, changePing] = React.useState('');
+  const [userID, setUserID] = useState('');
+
+  const dispatch = useDispatch();
+
+  const {messages} = useSelector(state => state.message);
+
+  useEffect(() => {
+    if (!userID) {
+      AsyncStorage.getItem('userDetails').then(userDetails => {
+        if (userDetails) {
+          setUserID(JSON.parse(userDetails).userID);
+
+          dispatch(
+            viewMessageData({
+              params: {
+                myID: JSON.parse(userDetails).userID,
+                otherID: props.route.params.item.id,
+              },
+            }),
+          );
+        }
+      });
+    }
+  });
+
+  const sendPing = () => {
+    if (ping) {
+      dispatch(
+        pingMessage({
+          body: {
+            sender: userID,
+            receiver: props.route.params.item.id,
+            message: ping,
+          },
+        }),
+      );
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.messageContainer}>
+        <ScrollView>
+          {messages?.allMessages?.length > 0 &&
+            messages?.allMessages.map(item => {
+              return <Card key={item.id} item={item} userID={userID} />;
+            })}
+        </ScrollView>
+      </View>
+
+      <View style={styles.pingConatiner}>
+        <TextInput
+          style={styles.customInput}
+          label="ping"
+          value={ping}
+          onChangeText={changePing}
+          placeholder="Enter message"
+        />
+        <TouchableOpacity onPress={sendPing}>
+          <Icon name="message" size={30} color={COLORS.primary} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  messageContainer: {
+    flexDirection: 'column',
+    gap: 20,
+    height: 500,
+  },
+  pingConatiner: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    gap: 30,
+    paddingHorizontal: 30,
+  },
+  customInput: {
+    width: 270,
+    marginVertical: 10,
+  },
+  card: {
+    width: 200,
+    height: 70,
+    padding: 10,
+    backgroundColor: 'aqua',
+    borderRadius: 30,
+    borderWidth: 2,
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 10,
+    justifyContent: 'space-between',
+  },
+});
 export default Message;
--- Demo/src/store/messageSlice.js
+++ Demo/src/store/messageSlice.js
@@ -25,8 +25,43 @@ export const viewMessageData = createAsyncThunk(
   },
 );
 
+export const pingMessage = createAsyncThunk(
+  'message/ping',
+  async ({body}, thunkAPI) => {
+    try {
+      const res = await api({
+        path: 'message/ping',
+        method: 'POST',
+        body,
+      });
+
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error?.message ?? MESSAGES.error_message);
+    }
+  },
+);
+
+export const deleteMessage = createAsyncThunk(
+  'message/delete',
+  async ({params, callback}, thunkAPI) => {
+    try {
+      const res = await api({
+        path: `message/delete/${params.messageID}`,
+        method: 'DELETE',
+      });
+
+      callback && callback();
+
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error?.message ?? MESSAGES.error_message);
+    }
+  },
+);
+
 const initialState = {
-  messages: [],
+  messages: {},
   isLoading: false,
   hasError: false,
 };
@@ -44,9 +79,32 @@ const messageSlice = createSlice({
       .addCase(viewMessageData.fulfilled, (state, action) => {
         state.isLoading = false;
         state.hasError = false;
-        state.allUsers = action.payload;
+        state.messages = action.payload;
       })
       .addCase(viewMessageData.rejected, state => {
+        state.isLoading = false;
+        state.hasError = true;
+      })
+      // Message pinged
+      .addCase(pingMessage.pending, state => {
+        state.hasError = false;
+      })
+      .addCase(pingMessage.fulfilled, state => {
+        state.hasError = false;
+      })
+      .addCase(pingMessage.rejected, state => {
+        state.hasError = true;
+      })
+      // Message delete
+      .addCase(deleteMessage.pending, state => {
+        state.isLoading = true;
+        state.hasError = false;
+      })
+      .addCase(deleteMessage.fulfilled, state => {
+        state.isLoading = false;
+        state.hasError = false;
+      })
+      .addCase(deleteMessage.rejected, state => {
         state.isLoading = false;
         state.hasError = true;
       });
