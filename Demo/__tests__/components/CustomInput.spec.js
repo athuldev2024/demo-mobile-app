e6cc8e369b4913a6b72017081a405d89d3ea28bd
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-24 01:42:38 +0530

    update unit test

---
+++ Demo/__tests__/components/CustomInput.spec.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {render, fireEvent, act, cleanup} from '@testing-library/react-native';
+import CustomInput from '../../src/components/common/CustomInput';
+import {Provider as PaperProvider} from 'react-native-paper';
+
+describe('CustomInput Component', () => {
+  const mockOnChangeText = jest.fn();
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    cleanup();
+    jest.clearAllMocks();
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  it('matches snapshot and renders properly', () => {
+    const {toJSON} = render(
+      <PaperProvider>
+        <CustomInput
+          placeholder="Enter your name"
+          value=""
+          onChangeText={jest.fn()}
+          touched={{}}
+          errors={{}}
+          fieldName="name"
+        />
+      </PaperProvider>,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+  it('see error message when invalid input', () => {
+    const {getByText} = render(
+      <PaperProvider>
+        <CustomInput
+          placeholder="Enter your name"
+          value=""
+          onChangeText={jest.fn()}
+          touched={{
+            name: 'touched',
+          }}
+          errors={{
+            name: 'error sample',
+          }}
+          fieldName="name"
+        />
+      </PaperProvider>,
+    );
+
+    expect(getByText('error sample')).toBeTruthy();
+  });
+});
