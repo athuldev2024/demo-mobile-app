f9a069f18dc53484ff17f1e63219549874488f91
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-23 19:44:53 +0530

    unit test initial file

--- Demo/jest.config.js
+++ Demo/jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@testing-library/react-native)/)',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  collectCoverage: true, // Enable coverage collection
+  collectCoverageFrom: ['src/components/**/*.{js,jsx,ts,tsx}'],
+  coverageReporters: ['text'], // Specify coverage report formats
 };
