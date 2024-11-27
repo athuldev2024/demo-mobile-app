55c14c9af6139bb2cdae908c3cc2b616fdbfcbdb
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-27 23:50:53 +0530

    updated finished unit test

--- Demo/__tests__/components/CustomButton.spec.js
+++ Demo/__tests__/components/CustomButton.spec.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {render, fireEvent} from '@testing-library/react-native';
 import CustomButton from '../../src/components/common/CustomButton';
 
 describe('CustomButton Component', () => {
-  it('matches snapshot and renders properly', () => {
-    const {getByText} = render(
+  it('renders a button with the correct title', () => {
+    const {getByRole, getByText} = render(
       <CustomButton title="Click Me" onPress={jest.fn()} />,
     );
 
+    const button = getByRole('button');
+    expect(button).toBeTruthy();
     expect(getByText('Click Me')).toBeTruthy();
+  });
+
+  it('calls the onPress function when the button is pressed', () => {
+    const pressEvent = jest.fn();
+    const {getByText} = render(
+      <CustomButton title="sample text" onPress={pressEvent} />,
+    );
+
+    const button = getByText('sample text');
+    fireEvent.press(button);
+
+    expect(pressEvent).toHaveBeenCalledTimes(1);
   });
 });
