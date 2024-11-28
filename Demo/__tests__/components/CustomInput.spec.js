636d4d76e6fca330ff7ad8eb98c1c875374fe8c9
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-28 16:25:49 +0530

    Finihsed unit test

--- Demo/__tests__/components/CustomInput.spec.js
+++ Demo/__tests__/components/CustomInput.spec.js
@@ -1,55 +1,62 @@
 import React from 'react';
-import {render, cleanup} from '@testing-library/react-native';
+import {render, fireEvent} from '@testing-library/react-native';
 import CustomInput from '../../src/components/common/CustomInput';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {PaperProvider} from 'react-native-paper';
+
+jest.mock('react-native-paper', () => {
+  const {TextInput, View, Text} = require('react-native');
+
+  return {
+    PaperProvider: ({children}) => <View>{children}</View>,
+    TextInput: jest.fn(
+      ({label, placeholder, value, onChangeText, editable}) => (
+        <View>
+          {label && <Text>{label}</Text>}
+          <TextInput
+            placeholder={placeholder}
+            value={value}
+            onChangeText={onChangeText}
+            editable={editable}
+          />
+        </View>
+      ),
+    ),
+  };
+});
 
 describe('CustomInput Component', () => {
-  beforeEach(() => {
-    jest.useFakeTimers();
-  });
+  const onChangeTextMock = jest.fn();
 
-  afterEach(() => {
-    cleanup();
-    jest.clearAllMocks();
-    jest.runOnlyPendingTimers();
-    jest.useRealTimers();
-  });
+  const renderWithPaperProvider = (props = {}) => {
+    const defaultProps = {
+      value: '',
+      onChangeText: onChangeTextMock,
+      placeholder: 'sample placeholder',
+      keyboardType: 'default',
+      secureTextEntry: false,
+      touched: {},
+      errors: {},
+      fieldName: 'name',
+    };
 
-  it('matches snapshot and renders properly', () => {
-    const {toJSON} = render(
+    return render(
       <PaperProvider>
-        <CustomInput
-          placeholder="Enter your name"
-          value=""
-          onChangeText={jest.fn()}
-          touched={{}}
-          errors={{}}
-          fieldName="name"
-        />
+        <CustomInput {...defaultProps} {...props} />
       </PaperProvider>,
     );
+  };
 
-    expect(toJSON()).toMatchSnapshot();
+  it('renders the TextInput and check if it exists', () => {
+    const {getByPlaceholderText} = renderWithPaperProvider();
+    expect(getByPlaceholderText('sample placeholder')).toBeTruthy();
   });
 
-  it('see error message when invalid input', () => {
-    const {getByText} = render(
-      <PaperProvider>
-        <CustomInput
-          placeholder="Enter your name"
-          value=""
-          onChangeText={jest.fn()}
-          touched={{
-            name: 'touched',
-          }}
-          errors={{
-            name: 'error sample',
-          }}
-          fieldName="name"
-        />
-      </PaperProvider>,
-    );
-
-    expect(getByText('error sample')).toBeTruthy();
+  it('renders the TextInput awith error message', () => {
+    const {getByText} = renderWithPaperProvider({
+      touched: {name: 'touched'},
+      errors: {name: 'name error message'},
+      fieldName: 'name',
+    });
+    expect(getByText('name error message')).toBeTruthy();
   });
 });
